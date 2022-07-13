d245a83c82e33f356286211e2151614456047ce1
Puffini <99602500+Puffini@users.noreply.github.com>
2022-07-13 17:49:40 +0300

    Delete bot directory

--- bot/mod_settings.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-
-files = ["config.py", "verification_mod.py", "choose_language_mod.py"]
-for file in files:
-    subprocess.Popen(args=["start", "python", file], shell=True, stdout=subprocess.PIPE)
